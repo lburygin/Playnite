484b095d968801c1782bc79d01a7955ecf73924e
Josef Nemec <nemecjosef@gmail.com>
2023-06-07 08:54:17 -0700

    Binding tools class hierarchy change

--- source7/Playnite/BindingTools.cs
+++ source7/Playnite/BindingTools.cs
@@ -119,21 +119,21 @@ public class BindingTools
     {
         BindingOperations.ClearBinding(target, dp);
     }
+}
 
-    public class BindingProxy : Freezable
+public class BindingProxy : Freezable
+{
+    protected override Freezable CreateInstanceCore()
     {
-        protected override Freezable CreateInstanceCore()
-        {
-            return new BindingProxy();
-        }
-
-        public object Data
-        {
-            get { return GetValue(DataProperty); }
-            set { SetValue(DataProperty, value); }
-        }
-
-        public static readonly DependencyProperty DataProperty =
-            DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
+        return new BindingProxy();
     }
+
+    public object Data
+    {
+        get { return GetValue(DataProperty); }
+        set { SetValue(DataProperty, value); }
+    }
+
+    public static readonly DependencyProperty DataProperty =
+        DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
 }
